1059d1924fd3353dc4a009c82062c5de043eafb1
Ganesh Rajole <ganeshrajole@gmail.com>
2022-11-09 12:51:33 +0530

    TrainBookingApp Ready to Deploy

---
+++ src/main/java/com/jdc/TrainBookingAppApplication.java
@@ -0,0 +1,16 @@
+package com.jdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainBookingAppApplication.class, args);
+		
+		
+		System.out.println("Train Ticket Booking Successfully Done");
+	}
+
+}
